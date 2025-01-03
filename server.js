f4205de418f63ff010297ca3ccb4113d31faac4f
no-support <bnine19302@gmail.com>
2025-01-03 14:05:58 +0900

    01_03_2 version

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
-// // 정적 파일 제공 (build 폴더)
-// app.use(express.static(path.join(__dirname, "build")));
+// 정적 파일 제공 (build 폴더)
+app.use(express.static(path.join(__dirname, "build")));
 
-// // 모든 요청을 React 앱으로 라우팅
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+// 모든 요청을 React 앱으로 라우팅
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
